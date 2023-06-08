15b1632d5c9f5ff72276d2985e782011f70d0ad6
Gaege Root <6657699+GaegeRoot@users.noreply.github.com>
2023-06-08 12:53:51 -0700

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import {Header} from './components/header.js';
+import {Footer} from './components/footer.js';
+import {CommonServiceBanner} from './components/common-service-banner.js';
+
+customElements.define('header-mmj', Header);
+customElements.define('footer-mmj', Footer);
+customElements.define('common-service-banner-mmj', CommonServiceBanner);
